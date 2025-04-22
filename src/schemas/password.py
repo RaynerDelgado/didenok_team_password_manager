cbaa440e9c6d9b6534c5b92ea3bd9bcecf82cf86
RaynerDelgado <59878855+RaynerDelgado@users.noreply.github.com>
2025-04-22 23:19:12 +0300

    change docs

--- src/schemas/password.py
+++ src/schemas/password.py
@@ -1,33 +1,25 @@
 """
-This module defines the data models and schemas related to tasks.
+This module defines the data models and schemas related to password.
 
-It includes classes and enums that represent the structure and validation rules 
-for task-related data, such as task status, task creation, task reading, and task updates.
+It includes classes that represent the structure and validation rules
+for password-related data, such as password creation, password reading.
 
 Classes:
-    - TaskStatus: An enumeration representing the possible statuses of a task.
-    - TaskCreate: A model representing the data required to create a new task.
-    - TaskRead: A model representing a task with additional details, inheriting from TaskCreate.
-    - TaskUpdate: A model representing the data required to update an existing task.
-    - TaskStatus: Defines the possible statuses of a task (NEW, IN_PROGRESS, COMPLETED).
+    - PasswordCreate: A model representing the data required to create a new password.
+    - PasswordRead: A model representing a password with additional details, inheriting from PasswordCreate.
 """
-from typing import Annotated, Optional
+from typing import Annotated
 
 from pydantic import BaseModel, Field
 
 
 class PasswordCreate(BaseModel):
     """
-    Class TaskCreate represents the data required to create a new task
+    Class PasswordCreate represents the data required to create a new password
 
     Args:
-        title (str): The title of the task, must be at least 4 characters long
-        description (str): A detailed description of the task (default is an empty string)
-        status (TaskStatus): The current status of the task (default is TaskStatus.NEW)
-        name (Optional[str]): An optional name associated with the task, must be at least 4 characters long
-
-    Raises:
-        ValueError: If title is less than 4 characters long
+        service_name (str): The name of the service, must be at least 2 characters long
+        password (str): The password of service, must be at least 8 characters long.
     """
     service_name: Annotated[str, Field(min_length=2)]
     password: Annotated[str, Field(min_length=8)]
@@ -35,13 +27,13 @@ class PasswordCreate(BaseModel):
 
 class PasswordRead(PasswordCreate):
     """
-    Class Task represents a task with additional details
+    Class PasswordRead represents a password with additional details
 
-    Inherits from TaskCreate and adds:
+    Inherits from PasswordCreate and adds:
 
     Args:
-        id (int): The unique identifier for the task
-        user_id (int): The identifier of the user who created the task
+        id (int): The unique identifier for the password.
+        password_hash (str): Hash of password.
     """
     id: int
     password_hash: str
